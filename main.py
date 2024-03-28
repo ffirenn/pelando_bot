60e447853ac059eee6c1c3b1517340e73b8f7656
firenn <69179554+firennn@users.noreply.github.com>
2024-03-28 11:09:51 -0300

    updated

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
-import requests
+import os
 import json
+import requests
 import time
 from bs4 import BeautifulSoup
 
-def send_offer(oferta):
+DATA_FILE = "last_offer_data.json"
+
+last_offer_data = {"title": "", "price": ""}
+
+def load_last_offer_data():
+    global last_offer_data
+
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as file:
+            last_offer_data = json.load(file)
+
+def save_last_offer_data():
+    global last_offer_data
+
+    with open(DATA_FILE, "w") as file:
+        json.dump(last_offer_data, file)
+
+def send_offer(offer):
+    global last_offer_data
+
     url = "http://localhost:3000/oferta"
 
-    dados_oferta = {
-        "title": oferta["title"],
-        "price": oferta["price"],
-        "image_url": oferta["image"]["url"],
-        "source_url": oferta["sourceUrl"],
-        "coupon_code": oferta.get("couponCode", "❌")
+    offer_data = {
+        "title": offer["title"],
+        "price": offer["price"],
+        "image_url": offer["image"]["url"],
+        "source_url": offer["sourceUrl"],
+        "coupon_code": offer.get("couponCode", "❌")
     }
 
-    response = requests.post(url, json=dados_oferta)
+    if offer_data["title"] != last_offer_data["title"] or offer_data["price"] != last_offer_data["price"]:
+        response = requests.post(url, json=offer_data)
 
-    if response.status_code == 200:
-        print("oferta enviada")
+        if response.status_code == 200:
+            print("Oferta enviada")
+            last_offer_data["title"] = offer_data["title"]
+            last_offer_data["price"] = offer_data["price"]
+            save_last_offer_data()
+        else:
+            print("Falha ao enviar a oferta. Código de status:", response.status_code)
     else:
-        print("Falha ao enviar a oferta. Código de status:", response.status_code)
+        print("A oferta é a mesma que a última oferta enviada. Não será enviada novamente.")
 
 def check_offers():
     url = "https://www.pelando.com.br/games-e-pc-gamer"
@@ -35,38 +61,40 @@ def check_offers():
         if script_tag:
             json_content = json.loads(script_tag.contents[0])
 
-            ofertas = json_content["props"]["pageProps"]["feedOffers"]["edges"]
+            offers = json_content["props"]["pageProps"]["feedOffers"]["edges"]
 
-            oferta_mais_recente = None
-            timestamp_mais_recente = None
+            latest_offer = None
+            latest_timestamp = None
 
-            for oferta in ofertas:
-                timestamp_oferta = oferta["timestamps"]["createdAt"]
-                if timestamp_mais_recente is None or timestamp_oferta > timestamp_mais_recente:
-                    oferta_mais_recente = oferta
-                    timestamp_mais_recente = timestamp_oferta
+            for offer in offers:
+                offer_timestamp = offer["timestamps"]["createdAt"]
+                if latest_timestamp is None or offer_timestamp > latest_timestamp:
+                    latest_offer = offer
+                    latest_timestamp = offer_timestamp
 
-            if oferta_mais_recente:
-                titulo = oferta_mais_recente["title"]
-                preco = oferta_mais_recente["price"]
-                url_imagem = oferta_mais_recente["image"]["url"]
-                url_oferta = oferta_mais_recente["sourceUrl"]
-                cupom_desconto = oferta_mais_recente["couponCode"] if "couponCode" in oferta_mais_recente and oferta_mais_recente["couponCode"] is not None else "❌"
+            if latest_offer:
+                title = latest_offer["title"]
+                price = latest_offer["price"]
+                image_url = latest_offer["image"]["url"]
+                source_url = latest_offer["sourceUrl"]
+                coupon_code = latest_offer["couponCode"] if "couponCode" in latest_offer and latest_offer["couponCode"] is not None else "❌"
 
                 print("Nova oferta encontrada!")
-                print("Título:", titulo)
-                print("Preço:", preco)
-                print("URL da Imagem:", url_imagem)
-                print("URL da Oferta:", url_oferta)
-                print("Cupom de Desconto:", cupom_desconto)
+                print("Título:", title)
+                print("Preço:", price)
+                print("URL da Imagem:", image_url)
+                print("URL da Oferta:", source_url)
+                print("Cupom de Desconto:", coupon_code)
 
-                send_offer(oferta_mais_recente)
+                send_offer(latest_offer)
             else:
                 print("Nenhuma nova oferta encontrada.")
         else:
             print("Não foi possível encontrar o elemento com o JSON.")
     else:
         print("Falha ao fazer a requisição. Código de status:", response.status_code)
+
+load_last_offer_data()
 
 while True:
     check_offers()
